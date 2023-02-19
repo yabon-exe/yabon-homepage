ecce6d7616e27ef8e52bad6a087cb0b75aaf5af0
yabon_exe <yabon116@gmail.com>
2023-02-20 01:31:08 +0900

    アイコン表示まで

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,198 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Top from './components/Top.vue'
 </script>
 
 <template> 
-  <!-- 
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it! おえ～～～～～" />
-    </div>
-  </header>
-  -->
-
   <main class="mainwrapper">
-    XXXXXXX
+    <Top />
   </main>
 
   
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss">
+  @charset "UTF-8";
+  @import url('https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap');
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  $fontcolor: #333;
+  $maincolor: #c0d9d3;
+  $subcolor: #ffee7c;
+  $basecolor: rgba(#faf4e8, 0.3);
+  $white: #fff;
+  $black: #000;
+  $twi: #1DA1F2;
+  $insta: #CF2E92;
+  $facebook: #4267b2;
+  $tumblr: #39475D;
+  $youtube: #DA1725;
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  $fontfamily: 'Zen Kaku Gothic New', YuGothic, 'Yu Gothic medium', 'Hiragino Sans', Meiryo, 'sans-serif';
+  $headingfont: 'Oooh Baby', 'Zen Kaku Gothic New', YuGothic, 'Yu Gothic medium', 'Hiragino Sans', Meiryo, 'sans-serif';
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  body {
+    font-family: $fontfamily;
+    font-size: 1.6rem;
+    color: $fontcolor;
+    letter-spacing: 2px;
+    line-height: 1.8;
+    background-color: $basecolor;
+
+    @media screen and (max-width: 520px) {
+      font-size: 1.4rem;
+    }
+
+    &::before,
+    &::after {
+      position: fixed;
+      left: 0;
+      content: "";
+      display: block;
+      width: 100%;
+      height: 20px;
+      background-image: linear-gradient(90deg, $maincolor 30%, $subcolor);
+      z-index: 3;
+    }
+
+    &::before {
+      top: 0;
+    }
+
+    &::after {
+      bottom: 0;
+    }
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .mainwrapper {
+    padding: 60px 10px;
+    >:first-child {
+      margin-top: 0;
+    }
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  /* PC */
+  @media screen and (min-width: 1025px),
+  print {
+    .mainwrapper {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 80px 20px;
+    }
   }
-}
+
+  p {
+    margin: 20px 0;
+    letter-spacing: 1px;
+    text-align: justify;
+  }
+
+  a {
+    color: inherit;
+  }
+
+/*－－－－－－－－－－ 見出し －－－－－－－－－－*/
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-family: $headingfont;
+    font-weight: normal;
+    letter-spacing: 4px;
+    text-align: center;
+    text-transform: uppercase;
+    word-break: break-all
+  }
+
+/*－－－－－－－－－－ 大見出し －－－－－－－－－－*/
+  .headingL {
+    font-size: clamp(3rem, 4rem, 4rem);
+    margin: 40px 0;
+    margin: clamp(3rem, 4rem, 4rem) 0;
+
+    &.ja {
+      font-size: clamp(2.4rem, 3rem, 3.2rem);
+    }
+  }
+
+/*－－－－－－－－－－ 中見出し －－－－－－－－－－*/
+  .headingM {
+    font-size: clamp(2.4rem, 2rem, 3rem);
+    margin: clamp(2rem, 3rem, 3rem) 0;
+
+    &.ja {
+      font-size: clamp(2rem, 2rem, 2.4rem);
+    }
+  }
+
+/*－－－－－－－－－－ 小見出し －－－－－－－－－－*/
+  .headingS {
+    font-size: clamp(2rem, 2rem, 2.2rem);
+    margin: 20px 0;
+
+    &.ja {
+      font-size: clamp(1.8rem, 2rem, 2rem);
+    }
+  }
+
+/*－－－－－－－－－－ 最小見出し －－－－－－－－－－*/
+  .headingSS {
+    font-size: clamp(1.8rem, 2rem, 2rem);
+    margin: 20px 0;
+  }
+
+/*－－－－－－－－－－ 文字装飾 －－－－－－－－－－*/
+  .right {
+    text-align: right;
+  }
+
+  .center {
+    text-align: center;
+  }
+
+  .marker {
+    background: linear-gradient(transparent 60%, $maincolor 60%);
+  }
+
+  .label {
+    display: block;
+    margin: 20px 0;
+    padding: 2px 10px;
+    background-color: $subcolor;
+    width: fit-content;
+  }
+
+
+/*－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+
+アイコン等
+
+－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－*/
+  .la,
+  .las,
+  .lab {
+    font-size: 1.2em;
+    color: $maincolor;
+    position: relative;
+    top: 2px;
+  }
+
+/* 矢印 */
+  .arrow {
+    width: 20px;
+    height: 20px;
+    border: 2px solid $maincolor;
+    border-bottom: 0;
+    border-left: 0;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+  }
 </style>
---
+++ src/components/TopIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- プロフィール画像 -->
+  <div class="profile__img"><img src="http://placehold.jp/400x400.png?text=sample" alt=""></div>
+  <!-- 名前 -->
+  <h1 class="myname">Game Demo<br/>Gallery</h1>
+</template>
+
+<style lang="scss">
+  .myname {
+    font-size: clamp(3rem, 6rem, 6rem);
+    margin-top: -1.6em;
+  }
+
+  .profile__img {
+    padding: 0 40px;
+
+    img {
+      border-radius: 50%;
+      -webkit-border-radius: 50%;
+      -moz-border-radius: 50%;
+      -ms-border-radius: 50%;
+      -o-border-radius: 50%;
+      margin: auto;
+    }
+  }
+</style>
